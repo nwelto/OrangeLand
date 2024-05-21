b295f6b1451cd89f3c3ea93458719523f9103e15
Nathan <nathopp@gmail.com>
2024-05-20 21:21:01 -0500

    User Methods Complete

---
+++ DTO/UpdateUserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace OrangeLand.DTO
+{
+    public class UpdateUserDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Role { get; set; }
+    }
+}
